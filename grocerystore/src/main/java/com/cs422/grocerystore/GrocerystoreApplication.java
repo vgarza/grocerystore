74678cc195743306c47da87d76ff3f84baac9a33
Alexander Lerma <alexanderrlerma@gmail.com>
2016-12-03 23:21:42 -0600

    removed uneccessary files

---
+++ grocerystore/src/main/java/com/cs422/grocerystore/GrocerystoreApplication.java
@@ -0,0 +1,12 @@
+package com.cs422.grocerystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrocerystoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrocerystoreApplication.class, args);
+	}
+}
---
+++ grocerystore/src/test/java/com/cs422/grocerystore/GrocerystoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cs422.grocerystore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GrocerystoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
