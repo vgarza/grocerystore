e9c92982d307cf482f94a4cab2a218676a136395
Alexander Lerma <alexanderrlerma@gmail.com>
2016-12-08 02:38:49 -0600

    more view stuff

---
+++ grocerystore/src/main/java/com/cs425/grocerystore/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.cs425.grocerystore.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by alexanderlerma on 12/8/16.
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("/static/");
+    }
+
+}
